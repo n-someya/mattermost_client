4560fec7adc0370b1cd77895d10c218f5dc8b7a7
Someya Naoki <someya.naoki@jp.fujitsu.com>
2016-07-07 16:11:47 +0900

    RENAME: mattermost -> mattermost_client

--- test/test_mattermost.py
+++ test/test_mattermost.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from nose.tools import eq_
-from mattermost import mattermost_cl
+from mattermost_client import mattermost_cl
 
 mmc = mattermost_cl.MatterMostClient("http://10.125.75.11:8065/hooks/qs4uzy4b1py98qjpiut93sewzw")
 
