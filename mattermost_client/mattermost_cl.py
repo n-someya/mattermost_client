808fd5ff3712de1e6787b4b16fe87194a3724752
n-someya <n.someya.ynu@gmail.com>
2016-07-11 14:15:48 +0200

    FIX: Add icon_emoji.

--- mattermost_client/mattermost_cl.py
+++ mattermost_client/mattermost_cl.py
@@ -11,11 +11,15 @@ class MatterMostClient(object):
     def __init__(self, url):
         self.__mattermosturl = url
 
-    def send_message(self, message, username='PythonClient', icon_url=None):
+    def send_message(self, message, username='PythonClient',
+                     icon_url=None, icon_emoji=None):
         """
         :param message: message which you want to send.
         :param username: username shown on chat.(default: PythonClient)
-        :param icon_url: icon shown on chat. you specify url for image.(default: None)
+        :param icon_url: icon shown on chat. you specify
+                            url for image.(default: None)
+        :param icon_emoji: icon shown on chat.
+                            specify :emoji: (default: None)
 
          Usage::
 
@@ -30,8 +34,11 @@ class MatterMostClient(object):
                 }
         if icon_url is not None:
             payload['icon_url'] = icon_url
+        if icon_emoji is not None:
+            payload['icon_emoji'] = icon_emoji
 
-        resp = requests.post(self.__mattermosturl, headers=header, data=json.dumps(payload))
+        resp = requests.post(self.__mattermosturl,
+                             headers=header, data=json.dumps(payload))
 
         if resp.status_code >= 400:
 
