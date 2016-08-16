1ab89b38629a002c48f758b4e4f6a1e4be4baf0c
n-someya <n.someya.ynu@gmail.com>
2016-08-16 17:28:46 +0200

    FIX: sample -> mattermost_client

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='mattermost_client',
     version='0.0.1',
     description='Mattermost(and Slack) incommig webhook client',
     long_description=readme,
